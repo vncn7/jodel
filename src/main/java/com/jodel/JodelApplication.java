feece7cd8c8ada05cddee74e4d71164f51be6c46
vncn7 <vncn7@protonmail.com>
2024-12-02 20:51:22 +0100

    Initial commit

---
+++ src/main/java/com/jodel/JodelApplication.java
@@ -0,0 +1,13 @@
+package com.jodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jodel/JodelApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.jodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
